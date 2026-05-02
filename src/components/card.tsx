5ea112dd6e1bdfa053afc6403dbbe554137e4fe3
Play Your Photos <douglas.cameron@delarue.org.au>
2026-05-02 12:48:01 +1000

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,1 +1,53 @@
+import React from "react";
 
+type CardProps = {
+  rank: string;
+  suit: "hearts" | "diamonds" | "spades" | "clubs";
+  image: string;
+  faceDown?: boolean;
+};
+
+const suitSymbols = {
+  hearts: "♥",
+  diamonds: "♦",
+  spades: "♠",
+  clubs: "♣",
+};
+
+const suitColors = {
+  hearts: "#ff4d4d",
+  diamonds: "#ff4d4d",
+  spades: "#ffffff",
+  clubs: "#ffffff",
+};
+
+export default function Card({
+  rank,
+  suit,
+  image,
+  faceDown = false,
+}: CardProps) {
+  return (
+    <div className="card">
+      <img
+        src={faceDown ? "/cards/demo-hearts/back.jpg" : image}
+        className="card-img"
+        alt={`${rank} of ${suit}`}
+      />
+
+      {!faceDown && (
+        <>
+          <div className="corner top-left" style={{ color: suitColors[suit] }}>
+            {rank}
+            <span>{suitSymbols[suit]}</span>
+          </div>
+
+          <div className="corner bottom-right" style={{ color: suitColors[suit] }}>
+            {rank}
+            <span>{suitSymbols[suit]}</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
