7fc482884b4ddefb9b333952ebf87cfe8998e9b2
Play Your Photos <douglas.cameron@delarue.org.au>
2026-05-02 12:50:13 +1000

    Update demoDeck.ts

--- src/data/demoDeck.ts
+++ src/data/demoDeck.ts
@@ -1,1 +1,9 @@
+export const ranks = [
+  "A","2","3","4","5","6","7","8","9","10","J","Q","K"
+];
 
+export const demoDeck = ranks.map((rank) => ({
+  rank,
+  suit: "hearts",
+  image: `/cards/demo-hearts/${rank}.jpg`,
+}));
