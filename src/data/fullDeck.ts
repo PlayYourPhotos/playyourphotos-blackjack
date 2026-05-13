fc8a4dc1c5079c35e21b3557eb0175c4fdd63cc5
Play Your Photos <douglas.cameron@delarue.org.au>
2026-05-13 12:52:30 +1000

    Update fullDeck.ts

--- src/data/fullDeck.ts
+++ src/data/fullDeck.ts
@@ -1,1 +1,44 @@
+const suits = [
+  {
+    suit: "hearts",
+    folder: "valkyra-hearts",
+  },
+  {
+    suit: "diamonds",
+    folder: "valkyra-diamonds",
+  },
+  {
+    suit: "spades",
+    folder: "valkyra-spades",
+  },
+  {
+    suit: "clubs",
+    folder: "valkyra-clubs",
+  },
+];
 
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+const fullDeck = suits.flatMap(({ suit, folder }) =>
+  ranks.map((rank) => ({
+    rank,
+    suit,
+    image: `/cards/${folder}/${rank}.jpg`,
+  }))
+);
+
+export default fullDeck;
