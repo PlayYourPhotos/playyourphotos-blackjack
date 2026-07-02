d6b53e2680451735f67b4f5743aa75aa2477727a
Play Your Photos <douglas.cameron@delarue.org.au>
2026-07-02 17:36:30 +1000

    Update StatusPanel.jsx

--- src/components/blackjack/StatusPanel.jsx
+++ src/components/blackjack/StatusPanel.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+export default function StatusPanel({
+  message,
+  playerTotal,
+  dealerTotal,
+}) {
+  return (
+    <div className="status-box">
+      <strong>{message}</strong>
+
+      <div>Player: {playerTotal}</div>
+
+      <div>Dealer: {dealerTotal}</div>
+    </div>
+  );
+}
