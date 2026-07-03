0ca81207bfaaa74ddf9333ada07a879469b72cc7
Play Your Photos <douglas.cameron@delarue.org.au>
2026-07-03 18:11:05 +1000

    Update DealerHand.jsx

--- src/components/blackjack/DealerHand.jsx
+++ src/components/blackjack/DealerHand.jsx
@@ -1,1 +1,27 @@
+import React from "react";
+import Card from "../Card";
 
+export default function DealerHand({
+  dealerHand,
+  dealerRevealed,
+  onOpenGallery,
+}) {
+  return (
+    <section className="hand-section dealer-section">
+      <h2>Dealer</h2>
+
+      <div className="hand-row dealer-hand-row">
+        {dealerHand.map((card, index) => (
+          <Card
+            key={`${card.rank}-${card.suit}-${index}`}
+            rank={card.rank}
+            suit={card.suit}
+            image={card.image}
+            faceDown={index === 1 && !dealerRevealed}
+            onClick={() => onOpenGallery(dealerHand, index)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
